f5c376b16840041a94be2c0268e032401cbc87b9
vrotondo <150374510+vrotondo@users.noreply.github.com>
2025-03-05 17:01:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,34 @@ module.exports = {
     // avg_temperature_in_fahrenheit,
     // avg_temperature_in_celsius
 };
+
+const day1TempF = 32;
+const day2TempC = 25;
+const day3TempF = 70;
+const day4TempC = 18;
+const day5TempF = 80;
+const day6TempC = 15;
+const day7TempF = 72;
+const day8TempC = 28;
+const day9TempF = 68;
+const day10TempC = 20;
+const day11TempF = 75;
+const day12TempC = 23;
+const day13TempF = 82;
+const day14TempC = 30;
+const day15TempF = 65;
+const day16TempC = 22;
+const day17TempF = 77;
+const day18TempC = 26;
+const day19TempF = 78;
+const day20TempC = 24;
+const day21TempF = 73;
+const day22TempC = 21;
+const day23TempF = 79;
+const day24TempC = 27;
+const day25TempF = 71;
+const day26TempC = 19;
+const day27TempF = 74;
+const day28TempC = 17;
+const day29TempF = 76;
+const day30TempC = 29;
